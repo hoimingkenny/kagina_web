1368e41475d9bf0dea03dd67b9692b851c24d6d9
hoimingkenny <chenghoiming@gmail.com>
2020-08-21 22:39:07 +0800

    end of today

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-// Jquery Smooth Scroll
-
-// $("#hero a").on("click", function(e) {
-//   if(this.hash !== ""){
-//     e.preventDefault();
-
-//     const hash = this.hash;
-
-//     $("html, body").animate(
-//       {
-//         scrollTop: $(hash).offset().top
-//       },
-//       800
-//     );
-//   }
-// });
-
+// Smooth Scroll
 const scroll = new SmoothScroll('#hero a[href*="#"]', {
   speed: 800
 });
 
-
+// Slick
 $(document).ready(function(){
   $('.post-wrapper').slick({
     slidesToShow: 1,
@@ -29,7 +13,32 @@ $(document).ready(function(){
     nextArrow: $(".next"),
     prevArrow: $(".prev")
   });
-    
-		
 });
 
+//Humburger Meun
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_meun = document.querySelector('.header .nav-bar .nav-list ul');
+const meun_item = document.querySelector('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', ()=>{
+  hamburger.classList.toggle('active'); 
+  mobile_meun.classList.toggle('active');
+})
+
+// Meun Bar Darken
+document.addEventListener('scroll', ()=>{
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250){
+    header.style.backgroundColor = '#5B7781'
+  } else{
+    header.style.backgroundColor = 'transparent'
+  }
+})
+
+meun_item.forEach(item => {
+  item.addEventListener('click', () =>{
+    hamburger.classList.toggle('active'); 
+    mobile_meun.classList.toggle('active');
+  })
+})
