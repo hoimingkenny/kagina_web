9426918ec0ac3db6ad2a333491b972ac380ae05d
hoimingkenny <chenghoiming@gmail.com>
2020-08-21 18:27:22 +0800

    slick

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const scroll = new SmoothScroll('#hero a[href*="#"]', {
 
 $(document).ready(function(){
   $('.post-wrapper').slick({
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
