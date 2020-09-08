23beedebdcc42966adb8bf16a1385b7ae1b39ba4
hoimingkenny <chenghoiming@gmail.com>
2020-09-08 11:12:44 +0800

    .

--- main.js
+++ main.js
@@ -16,7 +16,18 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     nextArrow: $(".next"),
     prevArrow: $(".prev"),
-  });
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+    ]
+  })
 });
 
 //Humburger Meun
@@ -24,7 +35,8 @@ const hamburger = document.querySelector(
   ".header .nav-bar .nav-list .hamburger"
 );
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul a");
+const meun_item_hover = document.querySelectorAll("#header .nav-list ul li:hover a");
 const header = document.querySelector(".header.container");
 const home_button = document.querySelector(".header-brand img");
 const button_bar = document.querySelector("#button-bar .button-bar");
@@ -44,6 +56,13 @@ document.addEventListener("scroll", () => {
     header.style.transition = "background 1s";
     button_bar.style.visibility = "visible";
     button_bar.style.opacity = "1";
+    menu_item.forEach((item) => {
+      item.style.color = "white";
+    })
+    meun_item_hover.forEach((item) => {
+      item.style.color = "red";
+    })
+    console.log(menu_item);
   
   } else {
     header.style.backgroundColor = 'transparent';
@@ -52,6 +71,9 @@ document.addEventListener("scroll", () => {
       "url('./background/background.jpg')";
     button_bar.style.visibility = "hidden";
     button_bar.style.opacity = "0";
+    menu_item.forEach((item) => {
+      item.style.color = "#5B7781";
+    })
   }
 });
 
@@ -74,4 +96,4 @@ card.forEach((item) => {
   item.addEventListener('click', function(){
     item.classList.toggle('is-flipped');
   })
-})
+});
