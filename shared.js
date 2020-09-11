0401b57aa5c8578ae9b7d9720d0318bbdb20b407
hoimingkenny <chenghoiming@gmail.com>
2020-09-11 16:16:00 +0800

    Update shared.js

--- shared.js
+++ shared.js
@@ -26,7 +26,7 @@ document.addEventListener("scroll", () => {
     header.style.backgroundColor = "#5B7781";
     header.style.transition = "background 1s";
 
-    button_bar.style.height = '50px';
+    button_bar.style.height = '46px';
 
     menu_item.forEach((item) => {
       item.style.color = "white";
