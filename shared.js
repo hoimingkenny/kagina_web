c58abf9f538c62832cfcdec8d024462593a7aec9
hoimingkenny <chenghoiming@gmail.com>
2020-09-11 15:40:18 +0800

    .

--- shared.js
+++ shared.js
@@ -4,9 +4,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 //Humburger Meun
-const hamburger = document.querySelector(
-  ".header .nav-bar .nav-list .hamburger"
-);
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul a");
 const meun_item_hover = document.querySelectorAll("#header .nav-list ul li:hover a");
@@ -31,10 +29,10 @@ document.addEventListener("scroll", () => {
     button_bar.style.opacity = "1";
     menu_item.forEach((item) => {
       item.style.color = "white";
-    })
+    });
     meun_item_hover.forEach((item) => {
       item.style.color = "red";
-    })
+    });
   
   } else {
     header.style.backgroundColor = 'transparent';
@@ -54,8 +52,6 @@ $(document).ready(function(){
   button_bar.style.opacity = "0";
 });
 
-
-
 // Close Hamburger when click
 menu_item.forEach((item) => {
   item.addEventListener("click", () => {
@@ -65,31 +61,38 @@ menu_item.forEach((item) => {
 });
 
 // Floating action button
-var action_call = document.querySelector('.button-bar .call');
-var action_map = document.querySelector('.button-bar .googleMap');
-  action_call.addEventListener('click', () => {
-    if ($(action_map).hasClass('active')){
-      action_map.classList.toggle('active');
-      action_call.classList.toggle('active');
+var call_button = document.querySelector('.button-bar .call');
+var map_button = document.querySelector('.button-bar .googleMap');
+  call_button.addEventListener('click', () => {
+    if ($(map_button).hasClass('active')){
+      map_button.classList.toggle('active');
+      call_button.classList.toggle('active');
     } else{
-      action_call.classList.toggle('active');
+      call_button.classList.toggle('active');
     }
   });
 
-  action_map.addEventListener('click', () => {
-    if ($(action_call).hasClass('active')){
-      action_call.classList.toggle('active');
-      action_map.classList.toggle('active');
+  map_button.addEventListener('click', () => {
+    if ($(call_button).hasClass('active')){
+      call_button.classList.toggle('active');
+      map_button.classList.toggle('active');
     } else{
-      action_map.classList.toggle('active');
+      map_button.classList.toggle('active');
     }
   });
 
   $(window).scroll(function(){
-    if ($(action_call).hasClass('active')){
-      action_call.classList.toggle('active');
-      // action_map.classList.toggle('active');
-    } else if  ($(action_map).hasClass('active')){
-      action_map.classList.toggle('active');
+    if ($(call_button).hasClass('active')){
+      call_button.classList.toggle('active');
+      // map_butto.classList.toggle('active');
+    } else if  ($(map_button).hasClass('active')){
+      map_button.classList.toggle('active');
     }
-  })
+  });
+
+// Button Bar
+var timeTable_button = document.querySelector('.button-bar .timetable');
+  timeTable_button.addEventListener('click', () => {
+    timeTable_button.classList.toggle('active');
+  });
+
